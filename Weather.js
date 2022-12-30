4b53f1a0f4dd606db2658f2477594189f52fd9c0
ts7752 <ts7752@naver.com>
2022-12-30 16:07:13 +0900

    master

--- Weather.js
+++ Weather.js
@@ -8,22 +8,32 @@ const weatherOptions = {
   Haze: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "안개",
+    subtitle: "안개가 짙습니다 운전을 조심하세요!",
   },
   Thunderstorm: {
     iconName: "weather-lightning",
-    gradient: ["#00c6fb", "#66a6ff"],
+    gradient: ["#373b44", "#4286f4"],
+    title: "천둥번개",
+    subtitle: "외출을 자제 하세요!",
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89f7fe", "#66a6ff"],
+    title: "이슬비",
+    subtitle: "이슬비",
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00c6fb", "#005bea"],
+    title: "비",
+    subtitle: "우산을 챙기세요!",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7de2fc", "#b9b6e5"],
+    title: "눈",
+    subtitle: "미끄러짐 주의!",
   },
   Atmosphere: {
     iconName: "weather-hail",
@@ -32,18 +42,26 @@ const weatherOptions = {
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#fef253", "#ff7300"],
+    title: "맑음",
+    subtitle: "날씨가 맑아요!",
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#d7d2cc", "#304352"],
+    title: "흐림",
+    subtitle: "구름이 있어 흐려요!",
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4da0b0", "#d39d38"],
+    title: "안개",
+    subtitle: "안개가 짙습니다 운전을 조심하세요!",
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4da0b0", "#d39d38"],
+    title: "미세먼지",
+    subtitle: "마스크를 꼭 챙기세요!",
   },
 };
 
@@ -64,7 +82,12 @@ export default function Weather({ temp, condition }) {
         />
         <Text style={styles.temp}>{temp}</Text>
       </View>
-      <View style={styles.halfcontainer} />
+      <View style={{ ...styles.halfcontainer, ...styles.textContainer }}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
     </LinearGradient>
   );
 }
@@ -99,5 +122,20 @@ const styles = StyleSheet.create({
   temp: {
     fontSize: 42,
     color: "white",
+  },
+  title: {
+    color: "white",
+    fontSize: "44",
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: "24",
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start",
   },
 });
